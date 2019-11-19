f5a4d8c147de6484dcc56108f2e927f13cd8cbdd
Ruth Álvarez <ruth.alvarez.diaz@gmail.com>
2019-11-19 12:14:41 +0100

    Unzip no option

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,8 +1,12 @@
 # This script should download the url specified in the first argument ($1), place it in the directory specified in the second argument ($2)
 wget -P $2 $1
+gunzip -k $2/*.gz
 
 # and *optionally* uncompress the downloaded file with gunzip if the third argument contains the word "yes".
-#if $3 -eq "yes"
+#if ["$3" == "yes"]
 #then
-#   wget -P $2 $1 | gunzip -k $2/*.fastq.gz
+#    echo "$3"
+#   wget -P $2 $1 | gunzip -k basename ls $2
+#else
+#   wget -P $2 $1
 #fi
