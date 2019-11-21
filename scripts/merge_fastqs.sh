a21fa040b988494995ee24136bc4873f29164f27
Ruth Álvarez <ruth.alvarez.diaz@gmail.com>
2019-11-21 12:03:27 +0100

    Merge script - zip result

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -1,3 +1,6 @@
 # This script should merge all files from a given sample (the sample id is provided in the third argument)
 # into a single file, which should be stored in the output directory specified by the second argument.
 # The directory containing the samples is indicated by the first argument.
+
+mkdir -p $2
+zcat $1/$3*.fastq.gz | gzip > $2/$3.fastq.gz
