b16c3f175a254b23fdbd0de6941ea89115f762ff
Ruth Álvarez <ruth.alvarez.diaz@gmail.com>
2019-11-21 12:00:15 +0100

    Download script -nc (no repite descarga)

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,6 +1,6 @@
 # This script should download the url specified in the first argument ($1), place it in the directory specified in the second argument ($2)
-wget -P $2 $1
-gunzip -k $2/*.gz
+wget -nc -P $2 $1
+gunzip -k -f $2/*.gz
 
 # and *optionally* uncompress the downloaded file with gunzip if the third argument contains the word "yes".
 #if ["$3" == "yes"]
