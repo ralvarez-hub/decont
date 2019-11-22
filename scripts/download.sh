cc39f8e22858dae087d5a577e32c256eee0b474e
Ruth Álvarez <ruth.alvarez.diaz@gmail.com>
2019-11-22 11:45:03 +0100

    Optional unzip -   :-@

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,12 +1,9 @@
 # This script should download the url specified in the first argument ($1), place it in the directory specified in the second argument ($2)
 wget -nc -P $2 $1
-gunzip -k -f $2/*.gz
 
 # and *optionally* uncompress the downloaded file with gunzip if the third argument contains the word "yes".
-#if ["$3" == "yes"]
-#then
-#    echo "$3"
-#   wget -P $2 $1 | gunzip -k basename ls $2
-#else
-#   wget -P $2 $1
-#fi
+
+if [ "$3" == "yes" ]
+then
+    gunzip -k -f $2/*.gz
+fi
